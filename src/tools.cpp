2909a0f2742925d32ce22f7c8bb0da83e98def09
landOfTigers <hanna.e.becker@gmail.com>
2018-12-28 21:01:40 +0000

    add error handling to rmse calculation

--- src/tools.cpp
+++ src/tools.cpp
@@ -12,8 +12,14 @@ Tools::Tools() {}
 Tools::~Tools() {}
 
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations, const vector<VectorXd> &ground_truth) {
-
   VectorXd rmse(4);
+  rmse << 0,0,0,0;
+
+  if (estimations.size() != ground_truth.size() || estimations.empty()) {
+    cout << "Invalid estimation or ground_truth data" << endl;
+    return rmse;
+  }
+
   for (unsigned int i=0; i < estimations.size(); ++i) {    
     VectorXd residual = estimations[i] - ground_truth[i];
     residual = residual.array()*residual.array();
--- test/tools_test.cpp
+++ test/tools_test.cpp
@@ -1,7 +1,9 @@
 #include "../src/tools.cpp"
 #include <gtest/gtest.h>
  
-TEST(CalculateRMSETest, PositiveNos) { 
+Tools tools;
+
+TEST(CalculateRMSETest, CorrectCalculation) { 
   vector<VectorXd> estimations;  
   VectorXd estimate(4);
   estimate << 1.1, 2.2, 3.3, 4.4;
@@ -18,13 +20,49 @@ TEST(CalculateRMSETest, PositiveNos) {
    
   VectorXd rmseExpected(4);
   rmseExpected << 0.36055512754, 0.4472135955, 0.53851648071, 0.63245553203;
-  
-  Tools tools;
+
   VectorXd rmseActual = tools.CalculateRMSE(estimations, ground_truth);
   
   for (unsigned int i=0; i < estimations.size(); i++) {
     ASSERT_FLOAT_EQ(rmseExpected[i], rmseActual[i]);
   }
+}
+
+TEST(CalculateRMSETest, UnequalLengths) {
+  vector<VectorXd> estimations;  
+  VectorXd estimate(4);
+  estimate << 1.1, 2.2, 3.3, 4.4;
+  estimations.push_back(estimate);
+  estimate << 5.5, 6.6, 7.7, 8.8;
+  estimations.push_back(estimate);
+  
+  vector<VectorXd> ground_truth;
+  VectorXd gt(4);
+  gt << 1.0, 2.0, 3.0, 4.0;
+  ground_truth.push_back(gt);
+
+  VectorXd rmseEmpected(4);
+  rmseEmpected << 0,0,0,0;
+  
+  VectorXd rmseActual = tools.CalculateRMSE(estimations, ground_truth);
+
+  ASSERT_EQ(rmseEmpected, rmseActual);
+}
+
+TEST(CalculateRMSETest, OneHasZeroElements) {
+  vector<VectorXd> estimations;  
+  
+  vector<VectorXd> ground_truth;
+  VectorXd gt(4);
+  gt << 1.0, 2.0, 3.0, 4.0;
+  ground_truth.push_back(gt);
+
+  VectorXd rmseEmpected(4);
+  rmseEmpected << 0,0,0,0;
+  
+  VectorXd rmseActual = tools.CalculateRMSE(estimations, ground_truth);
+
+  ASSERT_EQ(rmseEmpected, rmseActual);
 }
  
 int main(int argc, char **argv) {
