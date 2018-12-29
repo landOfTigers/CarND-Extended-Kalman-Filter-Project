a693249b4d5bfaea84c9718c17d11e77357d92d9
landOfTigers <hanna.e.becker@gmail.com>
2018-12-29 15:31:24 +0000

    revert changes in CMakeLists.txt, only apply them when running setup-test-environment

--- setup-test-environment.sh
+++ setup-test-environment.sh
@@ -5,3 +5,18 @@ cd /usr/src/gtest
 sudo cmake CMakeLists.txt
 sudo make
 sudo cp *.a /usr/lib
+
+cd /home/workspace/CarND-Extended-Kalman-Filter-Project
+cat <<EOT >> CMakeLists.txt
+
+# GTest
+cmake_minimum_required(VERSION 2.6)
+ 
+# Locate GTest
+find_package(GTest REQUIRED)
+include_directories(${GTEST_INCLUDE_DIRS})
+ 
+# Link runTests with what we want to test and the GTest and pthread library
+add_executable(executeTests test/tools_test.cpp)
+target_link_libraries(executeTests ${GTEST_LIBRARIES} pthread)
+EOT
